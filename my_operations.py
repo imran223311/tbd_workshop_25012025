bc21be8cab05f206d3657a605e40d0669f1c46f4
salayoot-stdcmu <salayoot_singkamlo@cmu.ac.th>
2025-01-25 13:33:42 +0700

    create greeting from B

--- my_operations.py
+++ my_operations.py
@@ -1,22 +1,24 @@
 # Member A: edit this function.
 def add(num1: int, num2: int) -> None:
     # Edit code under this line
-    
+    res = num1 + num2
     print(num1, "+", num2, "is", res)
-    pass
+    return res
 
 
 # Member B: edit this function.
 def diff(num1: int, num2: int) -> None:
     # Edit code under this line
 
+    res = num1 - num2
     print(num1, "-", num2, "is", res)
-    pass
+    return res
 
 
 # Member C: edit this function.
 def mul(num1: int, num2: int) -> None:
     # Edit code under this line
 
+    res = num1 * num2
     print(num1, "*", num2, "is", res)
-    pass
+    return res
